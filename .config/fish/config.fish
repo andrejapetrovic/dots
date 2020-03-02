f2927ee9d5882e865556c9447c04f44954d00387
andrejapetrovic <andreja2092@gmail.com>
2020-03-02 23:51:04 +0100

    set up symlinks

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-function fish_greeting
-end
-
-set fish_color_command green
-set fish_color_error red 
-
-alias c='find ~/.config ~/dmenu-scripts ~/.vimrc -type f | egrep -vi "deezer|Code - OSS" | fzf | xargs -ro vim'
-
